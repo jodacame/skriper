9ce520255d3282c294245b7b2e1a356800ebe747
Jose Canchila <jodacame@gmail.com>
2024-12-16 17:02:42 +0000

    Added app to pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,5 +6,12 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
+    }, {
+        name: "app",
+        cwd: "./app",
+        script: './.output/server/index.mjs',
+        instances: 1,
+        autorestart: true,
+        watch: false,
     }]
 }
