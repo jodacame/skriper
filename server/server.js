f7d54e16ffc4ee052153c23086a87d09a57bff20
Jose Canchila <jodacame@gmail.com>
2024-12-16 17:11:22 +0000

    Changed env server port

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(process.env.PORT || 3000, async () => {
+app.listen(process.env.SERVER_PORT || 3000, async () => {
     await DB.init();
     Logger.success(`Server started on port ${process.env.PORT || 3000}`);
 });
