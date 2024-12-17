6b6efd847dfae3d3fd13802ce60501975c0f56ba
Jose Canchila <jodacame@gmail.com>
2024-12-17 13:50:55 +0000

    Fix

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import OpenAI from './utils/openai.js';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-// app.use(cors());
+app.use(cors());
 
 
 
