df1062cae1ace079538f86e8fec83f25b141a9cc
Jose Canchila <jodacame@gmail.com>
2024-12-17 13:41:19 +0000

    Icon updated and remove cors

--- server/server.js
+++ server/server.js
@@ -9,8 +9,7 @@ import OpenAI from './utils/openai.js';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-// / allow  all origins
-app.use(cors());
+// app.use(cors());
 
 
 
